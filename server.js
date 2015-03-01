9b0ae688a42b29eacc2f4841a76a2fbd3c6c43e3
Lindsey <lameyer@users.noreply.github.com>
2015-02-28 17:40:13 -0800

    made it a server

--- server.js
+++ server.js
@@ -1,31 +1,48 @@
 var request = require('sync-request');
 var cheerio = require('cheerio');
-var spawn = require('child_process').spawn;
+var express = require('express');
 
-var stores = JSON.parse(request('GET', 'http://www.walmart.com/search/store-availability?location=95073').getBody().toString());
+var app = express();
 
-var storeIds = stores.stores.map(function(x) {
-  return x.id;
-}).join(',');
+var getWalmartProducts = function(query, zipcode) {
 
-var search = process.argv.slice(2).join('+');
-var response = request('GET', 'http://www.walmart.com/search/?query=' + search + '&stores=' + storeIds+ '&redirect=false');
+  var stores = JSON.parse(request('GET', 'http://www.walmart.com/search/store-availability?location=' + zipcode).getBody().toString());
 
-$ = cheerio.load(response.getBody().toString());
+  var storeIds = stores.stores.map(function(x) {
+    return x.id;
+  }).join(',');
 
-var products = $('.js-tile').map(function() {
-  var product = {
-    title: $(this).find('.js-product-title').text(),
-    price: $(this).find('.price').text(),
-    url: 'http://www.walmart.com' + $(this).find('.js-product-title').attr('href'),
-    inStore: $(this).find('.fulfillment-container').text().search('store pickup') !== -1
-  }
-  return product;
-}).get().filter(function(product) {
-  return product.inStore;
-});
+  var response = request('GET', 'http://www.walmart.com/search/?query=' + query + '&stores=' + storeIds+ '&redirect=false');
 
-console.log(products);
+  $ = cheerio.load(response.getBody().toString());
 
-// spawn('open', ['http://www.walmart.com/search/?query=' + search + '&stores=' + storeIds+ '&redirect=false']);
-spawn('open', [products[0].url]);
+  var products = $('.js-tile').map(function() {
+    var product = {
+      title: $(this).find('.js-product-title').text(),
+      price: $(this).find('.price').text(),
+      url: 'http://www.walmart.com' + $(this).find('.js-product-title').attr('href'),
+      inStore: $(this).find('.fulfillment-container').text().search('store pickup') !== -1
+    }
+    return product;
+  }).get().filter(function(product) {
+    return product.inStore;
+  });
+
+  return products;
+
+};
+
+
+app.get('/', function (req, res) {
+  res.send(JSON.stringify(getWalmartProducts('dog toys', '95073')))
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
+
