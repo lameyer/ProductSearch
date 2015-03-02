051f9fecfd310ea91993a810fbfd8dcfd17474ef
Lindsey <lameyer@users.noreply.github.com>
2015-03-01 21:06:43 -0800

    sorted products by distance

--- products.js
+++ products.js
@@ -23,11 +23,19 @@ var getProducts = function(query, zipcode, radius, callback) {
         products.push(product);
       });
     });
-    products.sort(function(a,b) {
-      return a.store.distance - b.store.distance;
-    });
-    products = _.groupBy(products, function(product) {
+
+    products = _.values(_.groupBy(products, function(product) {
       return product.upc;
+    }));
+
+    products.forEach(function(productList){
+      productList.sort(function(a,b){
+        return a.store.distance - b.store.distance;
+      });
+    });
+
+    products.sort(function(a,b) {
+      return a[0].store.distance - b[0].store.distance;
     });
 
     callback(products);
