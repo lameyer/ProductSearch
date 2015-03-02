6ee94e7b5306dfe9158eb662af54dd0cda6a18b2
Lindsey <lameyer@users.noreply.github.com>
2015-03-01 20:22:05 -0800

    added search results and loading

--- public/client.js
+++ public/client.js
@@ -1,17 +1,26 @@
 var app = angular.module('ProductSearch',[]);
 app.controller('SearchController', function($http) {
   var controller = this;
+  controller.query = 'Call of Duty';
   controller.zipcode = '95073';
+  controller.isRunning = false;
+
   controller.submit = function() {
+    controller.isRunning= true;
+
     $http.get('/api/search.json', {
       params: {
         search: controller.query,
         zipcode: controller.zipcode
       }
+
     }).success(function(data){
       controller.searchResults = data;
+      controller.isRunning = false;
+
     }).error(function(data){
       alert("Something went wrong!");
+      controller.isRunning = false;
     });
   }
 });
